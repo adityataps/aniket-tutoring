8964d22a24d5795edeb679daa19af98372ada824
Aniket-More-19 <ani8007028801@gmail.com>
2024-10-19 00:29:43 +0530

    chore: updated styles for all screen sizes

--- client/aniket-portfolio/src/pages/projects/Projects.tsx
+++ client/aniket-portfolio/src/pages/projects/Projects.tsx
@@ -5,124 +5,96 @@ const Projects: React.FC = () => {
   return (
     <div className="projects-container">
       <div className="project">
-        <h2 className="project-title">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
-          modi!
-        </h2>
-        <div className="project-description">
-          <ul>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-          </ul>
+        <div>
+          <h2 className="project-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
+            modi!
+          </h2>
+          <div className="project-description">
+            <ul>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit eveniet similique porro corporis molestias praesentium
+                harum magni est quibusdam facilis.
+              </li>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit eveniet similique porro corporis molestias praesentium
+                harum magni est quibusdam facilis.
+              </li>
+            </ul>
+          </div>
         </div>
+        <h3 className="see-project">See Project</h3>
       </div>
       <div className="project">
-        <h2 className="project-title">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
-          cumque?
-        </h2>
-        <div className="project-description">
-          <ul>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-          </ul>
+        <div>
+          <h2 className="project-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+            cumque?
+          </h2>
+          <div className="project-description">
+            <ul>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit eveniet similique porro corporis molestias praesentium
+                harum magni est quibusdam facilis.
+              </li>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit eveniet similique porro corporis molestias praesentium
+                harum magni est quibusdam facilis.
+              </li>
+            </ul>
+          </div>
         </div>
+        <h3 className="see-project">See Project</h3>
       </div>
       <div className="project">
-        <h2 className="project-title">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
-          voluptate.
-        </h2>
-        <div className="project-description">
-          <ul>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-          </ul>
+        <div>
+          <h2 className="project-title">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+            voluptate.
+          </h2>
+          <div className="project-description">
+            <ul>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit eveniet similique porro corporis molestias praesentium
+                harum magni est quibusdam facilis.
+              </li>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit eveniet similique porro corporis molestias praesentium
+                harum magni est quibusdam facilis.
+              </li>
+            </ul>
+          </div>
         </div>
+        <h3 className="see-project">See Project</h3>
       </div>
       <div className="project">
-        <h2 className="project-title">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
-          impedit.
-        </h2>
-        <div className="project-description">
-          <ul>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-          </ul>
+        <div>
+          <h2 className="project-title">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
+            impedit.
+          </h2>
+          <div className="project-description">
+            <ul>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit eveniet similique porro corporis molestias praesentium
+                harum magni est quibusdam facilis.
+              </li>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit eveniet similique porro corporis molestias praesentium
+                harum magni est quibusdam facilis.
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="project">
-        <h2 className="project-title">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
-          impedit.
-        </h2>
-        <div className="project-description">
-          <ul>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="project">
-        <h2 className="project-title">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
-          impedit.
-        </h2>
-        <div className="project-description">
-          <ul>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-            <li>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-              eveniet similique porro corporis molestias praesentium harum magni
-              est quibusdam facilis.
-            </li>
-          </ul>
-        </div>
+        <h3 className="see-project">See Project</h3>
       </div>
     </div>
   );

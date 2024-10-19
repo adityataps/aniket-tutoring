7097fdf4a45964718cf18404e3d1741b62f051ca
Aniket-More-19 <ani8007028801@gmail.com>
2024-10-19 11:27:26 +0530

    chore: updated Resume page UI and styles

--- client/aniket-portfolio/src/pages/resume/Resume.tsx
+++ client/aniket-portfolio/src/pages/resume/Resume.tsx
@@ -3,10 +3,6 @@ import "./resume.css";
 export default function Resume() {
   return (
     <div className="resume-page">
-      <div className="download-resume">
-        <img src="./src/assets/images/pdf-icon.png" alt="pdf icon" />
-        <h3>Download My Resume</h3>
-      </div>
       <div className="resume-container">
         <iframe
           className="pdf"

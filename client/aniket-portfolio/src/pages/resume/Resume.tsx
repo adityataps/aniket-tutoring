4d972fdd871c3e3a5959db2011cc01e0e725ebe5
Aniket-More-19 <ani8007028801@gmail.com>
2024-10-19 10:55:02 +0530

    feat: added code for Resume page and styles for all screen sizes

--- client/aniket-portfolio/src/pages/resume/Resume.tsx
+++ client/aniket-portfolio/src/pages/resume/Resume.tsx
@@ -1,3 +1,18 @@
+import "./resume.css";
+
 export default function Resume() {
-  return <div>Resume Page</div>;
+  return (
+    <div className="resume-page">
+      <div className="download-resume">
+        <img src="./src/assets/images/pdf-icon.png" alt="pdf icon" />
+        <h3>Download My Resume</h3>
+      </div>
+      <div className="resume-container">
+        <iframe
+          className="pdf"
+          src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210101201653/PDF.pdf"
+        ></iframe>
+      </div>
+    </div>
+  );
 }
